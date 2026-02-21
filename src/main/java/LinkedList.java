174c2b4560b0bdaab8e50875bd6f44535d560e55
CardenDang <dangcarden@gmail.com>
2026-02-21 09:30:15 -0800

    Add Node class

--- src/main/java/LinkedList.java
+++ src/main/java/LinkedList.java
@@ -1,3 +1,19 @@
+
+/* Assignment 1 */
 public class LinkedList {
     
+    // 1. Node Class
+    public static class Node {
+        int key;
+        Node next;
+
+        Node(int key) {
+            this.key = key;
+            this.next = null;
+        }
+        @Override
+        public String toString() {
+            return String.valueOf(key);
+    }
+}
 }
