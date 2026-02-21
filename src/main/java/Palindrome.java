70a42d700d920055dd84ca40cd998b7d5e21e781
CardenDang <dangcarden@gmail.com>
2026-02-21 10:08:01 -0800

    Add node and check for palindrome

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -1,3 +1,54 @@
+import java.util.Arrays;
+
 public class Palindrome {
-    
+    /**
+     * Assignment 2: Palindrome
+     * Checks if singly linked list is a palindrome.
+     * Ex. 1 -> 2 -> 3 -> 2 -> 1
+     * 
+     */
+
+    /**
+     * Node class
+     */
+    static class Node {
+        int key;
+        Node next;
+
+        Node(int key) {
+            this.key = key;
+            this.next = null;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(key);
+        }
+    }
+
+    public static boolean isPalindrome(Node head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        // Find middle (slow ends at middle)
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // If odd length, slow is at the middle element
+        // We reverse from slow onwards (works for both even and odd)
+        Node secondHalfHead = reverseList(slow);
+
+        // Compare the first half and the reverse second half
+        Node p1 = head;
+        Node p2 = secondHalfHead;
+        boolean ok = true;
+        while (p2 != null) {
+
+        }
+    }
 }
